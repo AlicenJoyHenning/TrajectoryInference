d3e55004f8c3cc8ebb06e54737f5a7a18a53e650
Alicen Henning <129797527+AlicenJoyHenning@users.noreply.github.com>
2024-03-08 11:54:51 +0200

    Update WorkshopTI.R

--- WorkshopTI.R
+++ WorkshopTI.R
@@ -1,7 +1,7 @@
 # This R Script covers a workshop given by Bioconductor
 # https://kstreet13.github.io/bioc2020trajectories/articles/workshopTrajectories.html
 
-# [1] Install packages 
+# [1] Install packages ####
 BiocManager::install(c("slingshot", "SingleCellExperiment", "RColorBrewer",
                        "scales", "viridis", "UpSetR", "pheatmap", "msigdbr",
                        "fgsea", "knitr", "ggplot2", "gridExtra", "tradeSeq"))
@@ -26,7 +26,21 @@ install.packages("remotes")
 remotes::install_github("kstreet13/bioc2020trajectories")
 library(bioc2020trajectories)
 
-# [2] Upstream workflow 
+# [2] Upstream workflow ####
+getwd()
+setwd("C:/Users/alice/OneDrive/Documents/GitHub/bioc2020trajectories")
+# sce <- bioc2020trajectories::importRawData()
 
-sce <- bioc2020trajectories::importRawData()
-importRawData()
+# [3] Workflow Process ####
+# Load data
+data("sce", package = "bioc2020trajectories")
+
+# After integration, the first question is of differential topology: 
+# Q: are there very different topologies btw the 2 conditions 
+
+# creates a new variable 'shuffle' of integer value
+shuffe <- sample(ncol(sce)) 
+layout(matrix(1:2, nrow = 1))
+par(mar = c(4.5, 4, 1, 1))
+
+plot(reducedDims(sce)$UMAP)
